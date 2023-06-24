55d80f1bd902f98ce3ea039dd667e401f94ecde9
Segura <segura-deluna@mail.ru>
2023-06-24 05:26:35 +0300

    response weather data

--- scripts/modules/widgetService.js
+++ scripts/modules/widgetService.js
@@ -1,12 +1,20 @@
-import {renderWidgetForecast, renderWidgetOther, renderWidgetToday} from './render.js';
+import {renderWidgetForecast, renderWidgetOther, renderWidgetToday, showError} from './render.js';
+import {fetchWeather} from './APIservice.js';
 
-export const startWidget = () => {
+export const startWidget = async () => {
   const widget = document.createElement('div');
-  widget.classList.add('widget')
+  widget.classList.add('widget');
 
-  renderWidgetToday(widget);
-  renderWidgetOther(widget);
+  const dataWeather = await fetchWeather('Москва');
+
+  if (dataWeather.success) {
+    renderWidgetToday(widget, dataWeather.data);
+    renderWidgetOther(widget, dataWeather.data);
+  } else {
+    showError();
+  }
+
   renderWidgetForecast(widget);
 
   return widget;
-}
+};
