84de17f0fc0ca7c46e3eaececdae0120a30cf66f
Segura <segura-deluna@mail.ru>
2023-07-15 19:04:09 +0300

    Add search city

--- scripts/modules/widgetService.js
+++ scripts/modules/widgetService.js
@@ -1,11 +1,25 @@
-import { renderWidgetForecast, renderWidgetOther, renderWidgetToday, showError } from './render.js';
-import { fetchForecast, fetchWeather } from './APIservice.js';
+import {
+  renderWidgetForecast,
+  renderWidgetOther,
+  renderWidgetToday,
+  showError,
+} from './render.js';
+import { fetchForecast, fetchWeather, getCity } from './APIservice.js';
 
-export const startWidget = async () => {
-  const city = 'Москва';
+export const startWidget = async (city, widget) => {
+  if (!city) {
+    const dataCity = await getCity();
+    if (dataCity.success) {
+      city = dataCity.city;
+    } else {
+      showError(widget, dataCity.error);
+    }
+  }
 
-  const widget = document.createElement('div');
-  widget.classList.add('widget');
+  if (!widget) {
+    widget = document.createElement('div');
+    widget.classList.add('widget');
+  }
 
   const dataWeather = await fetchWeather(city);
 
@@ -13,7 +27,7 @@ export const startWidget = async () => {
     renderWidgetToday(widget, dataWeather.data);
     renderWidgetOther(widget, dataWeather.data);
   } else {
-    showError(dataWeather.error);
+    showError(widget, dataWeather.error);
   }
 
   const dataForecast = await fetchForecast(city);
@@ -21,7 +35,7 @@ export const startWidget = async () => {
   if (dataForecast.success) {
     renderWidgetForecast(widget, dataForecast.data);
   } else {
-    showError(dataForecast.error);
+    showError(widget, dataForecast.error);
   }
 
   return widget;
